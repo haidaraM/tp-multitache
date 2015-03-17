e17d93779c2250e69fe8751a9a3d77c45af858d5
Bai Emilien <ebai@if501-219-13.insa-lyon.fr>
2015-03-17 16:56:50 +0100

    Gestion de l'heure fonctionelle

--- Mere.cpp
+++ Mere.cpp
@@ -15,6 +15,9 @@
 #include "Mere.h"
 #include "Outils.h"
 #include "unistd.h"
+#include "Heure.h"
+#include "signal.h"
+#include "sys/wait.h"
 
 ///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
@@ -41,10 +44,23 @@
 //
 //----- fin de Nom
 int main (void)
-{
+{	
+	pid_t pidC;
 	InitialiserApplication(XTERM);
+	//Création de la tache Heure
+	
+	
+	pidC = CreerEtActiverHeure();
+	
+	
+		
 	sleep(10);
+	//envoi de sigusr2 à heure : commande de kill
+	kill( pidC , SIGUSR2);
+	//waitpid
+	waitpid(pidC, 0, 0);
 	TerminerApplication();
 	return 0;
+	
 	
 }
