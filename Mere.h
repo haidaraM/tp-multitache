09e6f92132ac5ecae262a54e5e5331e12dbeca02
Bai Emilien <ebai@if501-219-13.insa-lyon.fr>
2015-03-17 15:42:14 +0100

    l'absence d'arborescence

---
+++ Mere.h
@@ -0,0 +1,41 @@
+/*************************************************************************
+                           Mere  -  description
+                             -------------------
+    début                : 17/03/2015
+    copyright            : (C) Emilien Bai	
+    e-mail               : emilien.bai@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface de la tâche <Mere> (fichier Mere.h) -------
+#if ! defined ( Mere_H )
+#define Mere_H
+
+//------------------------------------------------------------------------
+// Rôle de la tâche <Mere>
+//Cette tache a pour role de lancer toutes les taches filles et de les 
+//supprimer proprement.
+//
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+// type Nom ( liste de paramètres );
+// Mode d'emploi :
+//
+// Contrat :
+//
+
+int main(void);
+//la fonction principale de notre application
+//
+// Contrat: aucun
+
+#endif // XXX_H
+
