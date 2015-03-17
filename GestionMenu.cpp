c3f94afe33e43b42d46e1f428e5c7009bd2faf87
elmhaidara <elmhaidara@gmail.com>
2015-03-17 17:20:43 +0100

    Makefile_local supprimé et ajout gestion menu

---
+++ GestionMenu.cpp
@@ -0,0 +1,58 @@
+/*************************************************************************
+                           GestionMenu  -  description
+                             -------------------
+    début                : GestionMenu
+    copyright            : (C) 2015 par Mohamed HAIDARA
+    e-mail               : GestionMenu
+*************************************************************************/
+
+//---------- Réalisation de la tâche <GestionMenu> (fichier GestionMenu.cpp) ---
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//-------------------------------------------------------- Include système
+
+//------------------------------------------------------ Include personnel
+#include "GestionMenu.h"
+#include "Menu.h"
+
+///////////////////////////////////////////////////////////////////  PRIVE
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+
+//------------------------------------------------------ Fonctions privées
+//static type nom ( liste de paramètres )
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+//{
+//} //----- fin de nom
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+void GestionMenu()
+// Algorithme :
+//
+{
+	Menu();
+} //----- fin de Nom
+
+void Commande ( char code )
+{
+
+}
+
+void Commande ( TypeVoie entree, TypeVoie sortie )
+{
+
+}
+
+void Commande ( TypeVoie voie, unsigned int duree )
+{
+	
+}
